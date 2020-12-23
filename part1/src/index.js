3b34712525652d8749f5fa179a2ef06fc34137ab
JamesAlexanderHill <jhill7177@gmail.com>
2020-12-23 18:46:37 +1100

    1.6: unicafe step1
     implemented a web application for collecting customer feedback. There are only three options for feedback: good, neutral, and bad.

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,65 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-const Content = (props) => {
-  const partList = props.parts.map(part => {
-    return <Part key={part.name.toString()} part={part.name} exercises={part.exercises} />
-  })
-  //console.log(partList);
+const Stats = () => {
   return (
     <div>
-      {partList}
+      <h1>Statistics</h1>
     </div>
   )
 }
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-const Total = (props) => {
-  const parts = props.parts;
-  let sum = 0;
-  parts.forEach(part => {
-    sum += part.exercises;
-  })              
-  return (
-    <p>Number of exercises {sum}</p>
-  )
-}
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodIncrement = () => {
+    setGood(good + 1)
+  }
+  const handleNeutralIncrement = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBadIncrement = () => {
+    setBad(bad + 1)
   }
 
   return (
     <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <h1>Give Feedback</h1>
+      <button onClick={handleGoodIncrement}>Good</button>
+      <button onClick={handleNeutralIncrement}>Neutral</button>
+      <button onClick={handleBadIncrement}>Bad</button>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
