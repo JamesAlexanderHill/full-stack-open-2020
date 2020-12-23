dbcc556142fe55979a4dc4667c6055eff2448c1b
JamesAlexanderHill <jhill7177@gmail.com>
2020-12-23 15:13:59 +1100

    1.4: course information step4
    Modified the variable definitions of App to use an array of parts and modified the application to work with new definitions

--- part1/src/index.js
+++ part1/src/index.js
@@ -7,15 +7,18 @@ const Header = (props) => {
   )
 }
 const Content = (props) => {
+  const partList = props.parts.map(part => {
+    return <Part key={part.name.toString()} part={part.name} exercises={part.exercises} />
+  })
+  console.log(partList);
   return (
     <div>
-      <Part part={props.part1.name} exercises={props.part1.exercises} />
-      <Part part={props.part2.name} exercises={props.part2.exercises} />
-      <Part part={props.part3.name} exercises={props.part3.exercises} />
+      {partList}
     </div>
   )
 }
 const Part = (props) => {
+  
   return (
     <p>
       {props.part} {props.exercises}
@@ -29,24 +32,26 @@ const Total = (props) => {
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total sum={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={parts}/>
+      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
     </div>
   )
 }
