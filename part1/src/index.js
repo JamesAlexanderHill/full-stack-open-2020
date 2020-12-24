ed23ab254734fceac7b56672fe91fab588b4013c
JamesAlexanderHill <jhill7177@gmail.com>
2020-12-24 11:22:57 +1100

    1.8: unicafe step3
    Refactored the application so that displaying the statistics is extracted into its own Statistics component.

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad;
+  return(
+    <div>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {(good - bad)/9}</p>
+      <p>Positive {((good)/(9)*100)}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -17,20 +31,14 @@ const App = () => {
     setBad(bad + 1)
   }
 
-  const all = good + neutral + bad;
-
   return (
     <div>
       <h1>Give Feedback</h1>
       <button onClick={handleGoodIncrement}>Good</button>
       <button onClick={handleNeutralIncrement}>Neutral</button>
       <button onClick={handleBadIncrement}>Bad</button>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {all}</p>
-      <p>Average {(good - bad)/9}</p>
-      <p>Positive {((good)/(9)*100)}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+      
     </div>
   )
 }
