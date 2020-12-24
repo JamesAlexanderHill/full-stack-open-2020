ee16db02295ae5f614ac91cdfe6070302f091983
JamesAlexanderHill <jhill7177@gmail.com>
2020-12-24 11:57:46 +1100

    1.12*: anecdotes step1
    Expanded the application by adding a button that can be clicked to display a random anecdote

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,65 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
+const Button = ({text, func}) => {
+  return(
+    <button onClick={func}>{text}</button>
   )
 }
-const Content = (props) => {
-  const partList = props.parts.map(part => {
-    return <Part key={part.name.toString()} part={part.name} exercises={part.exercises} />
-  })
-  //console.log(partList);
-  return (
-    <div>
-      {partList}
-    </div>
-  )
-}
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-const Total = (props) => {
-  const parts = props.parts;
-  let sum = 0;
-  parts.forEach(part => {
-    sum += part.exercises;
-  })              
-  return (
-    <p>Number of exercises {sum}</p>
-  )
-}
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+  const handleSetSelected = () => {
+    let rand = Math.floor(Math.random() * 5);
+    while(rand == selected){
+      rand = Math.floor(Math.random() * 5);
+    }
+    setSelected(rand);
   }
 
   return (
     <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      {props.anecdotes[selected]}
+      <br />
+      <Button text="New Anecdote" func={handleSetSelected} />
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
