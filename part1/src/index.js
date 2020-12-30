b5a88efc506b361635fc1d0beda454a923850075
JamesAlexanderHill <jhill7177@gmail.com>
2020-12-30 17:08:07 +1100

    2.12* Data for countries, step1
    Createed an application, in which one can look at data of various countries.

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,60 +1,78 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
-import Course from './components/Course'
+import axios from 'axios'
 
-
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-  const data = courses.map(course =>
-    <Course key={course.id} course={course} />
+const Country = (props) => {
+  const country = props.country;
+  const languages = country.languages.map((lang) => <li key={lang.name}>{lang.name}</li>);
+  return(
+    <div>
+      <h1>{country.name}</h1>
+      <p>
+        capital {country.capital}<br />
+        population {country.population}
+      </p>
+      <h3>Languages</h3>
+      <ul>
+        {languages}
+      </ul>
+      <img src={country.flag} width="200px"/>
+    </div>
   )
+}
+
+const Countries = (props) => {
+  const countries = props.countries;
+  const list = countries.map((country) => <li key={country.numericCode}>{country.name}</li>);
+  if(countries.length > 10){
+    return(
+      <p>Too many matches, specify another filter</p>
+    )
+  }else if(countries.length <= 0){
+    return(
+      <p>No countries match this filter</p>
+    )
+  }else if(countries.length === 1){
+    return(
+      <Country country={countries[0]} />
+    )
+  }else{
+    return(
+      <ul>
+        {list}
+      </ul>
+    )
+  }
+  
+}
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  const handleSetFilter = (event) => {
+    event.preventDefault();
+    setFilter(event.target.value)
+  }
+
+  const addCountries = (countryArr) => {
+    setCountries(countryArr);
+  }
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log(response.data);
+        addCountries(response.data)
+      })
+  }, [])
+
+  const filteredCountries = countries.filter((country) => country.name.toLowerCase().includes(filter.toLowerCase()));
+
   return (
     <div>
-      <h1>Web development curricilum</h1>
-      {data}
+      Find countries <input value={filter} onChange={handleSetFilter} />
+      <Countries countries={filteredCountries}/>
     </div>
   )
 }
