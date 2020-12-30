17f34facc7bf2c77d11f042203ca30024d9f49eb
JamesAlexanderHill <jhill7177@gmail.com>
2020-12-30 14:12:25 +1100

    2.8: The Phonebook Step3
    Expanded the application by allowing users to add phone numbers to the phone book

--- part1/src/components/App.js
+++ part1/src/components/App.js
@@ -7,16 +7,19 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault();
     const personObj = {
-        name: newName
+        name: newName,
+        number: newNumber
     };
     const index = persons.findIndex(person => person.name == personObj.name)
     if(index == -1){
         setPersons(persons.concat(personObj));
         setNewName('');
+        setNewNumber('');
     }else{
         alert(`${newName} is already added to phonebook`);
     }
@@ -25,19 +28,19 @@ const App = () => {
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  }
   const numbers = persons.map((person) => {
-      return <li key={person.name}>{person.name}</li>
+      return <li key={person.name}>{person.name} - {person.number}</li>
   });
   return (
     <div>
         <h2>Phonebook</h2>
         <form onSubmit={addPerson}>
-            <div>
-                name: <input value={newName} onChange={handleNameChange} />
-            </div>
-            <div>
-                <button type="submit">add</button>
-            </div>
+            <div>name: <input value={newName} onChange={handleNameChange} /></div>
+            <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+            <div><button type="submit">add</button></div>
         </form>
         <h2>Numbers</h2>
         <ul>
