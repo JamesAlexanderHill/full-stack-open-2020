be7de0666140131e74ab7d3a60d402a5164f7c34
JamesAlexanderHill <jhill7177@gmail.com>
2020-12-30 12:57:16 +1100

    2.1: Course information step6
    Defined a component responsible for formatting a single course called Course.

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
@@ -36,30 +45,27 @@ const Total = (props) => {
 }
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
-
-  return (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
+  return <Course course={course} />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
